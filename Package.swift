ee53e168b0d3cf33b4bf11d051dc48cbc1464ae6
Georgy Stepanov <stepanov.georgy.4@gmail.com>
2023-08-18 18:38:37 +0200

    added supported platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SimpleNetworkService",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14),
+        .macOS(.v12),
+        .tvOS(.v11),
+        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
